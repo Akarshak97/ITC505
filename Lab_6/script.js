e582f18206656c316181b0b55fe489a17fbe73a9
Akarshak97 <as6726@nau.edu>
2025-10-27 18:54:43 -0700

    script.js

--- Lab_6/script.js
+++ Lab_6/script.js
@@ -1,9 +1,11 @@
 document.getElementById("sortBtn").addEventListener("click", function() {
-  let arr = [5, 3, 8, 4, 2];
-  let len = arr.length;
-
-  for (let i = 0; i < len - 1; i++) {
-    for (let j = 0; j < len - i - 1; j++) {
+  const input = document.getElementById("arrayInput").value;
+  // parse into array of numbers
+  let arr = input.split(",").map(s => parseInt(s.trim())).filter(n => !isNaN(n));
+  
+  // simple bubble‐sort (or use built-in sort)
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
