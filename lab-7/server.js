2e2e3908f96f629820e7aaa7dc258f919ee6306c
Akarshak97 <as6726@nau.edu>
2025-11-10 21:33:03 -0700

    Update server.js

--- lab-7/server.js
+++ lab-7/server.js
@@ -4,26 +4,21 @@ const path = require('path')
 const server = express()
 
 server.use(express.urlencoded({ extended: true }))
-server.use(express.json())  // to parse JSON body
+server.use(express.json())
 server.use(logger('dev'))
 
+// Serve static files
 const publicServedFilesPath = path.join(__dirname, 'public')
 server.use(express.static(publicServedFilesPath))
 
-server.post('/ITC505/lab-7/index.html', (req, res) => {
+// POST endpoint for Mad Lib submission
+server.post('/submitMadLib', (req, res) => {
   const { name, adjective, place, animal, verb } = req.body
-
   if (!name || !adjective || !place || !animal || !verb) {
-    res.json({ error: 'Please fill in all the fields.' })
-    return
+    return res.json({ error: 'Please fill in all the fields.' })
   }
-
   const story = `One day, ${name} went to ${place}. There they met a ${adjective} ${animal} that loved to ${verb} all day long!`
   res.json({ story })
-})
-
-server.get('/do_a_random', (req, res) => {
-  res.send(`Your number is: ${Math.floor(Math.random() * 100) + 1}`)
 })
 
 let port = 80
